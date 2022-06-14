a806eb3a03d029031531bc580b70b9976646d908
ayoeze191 <olabodeezekiel2018@yahoo.com>
2022-06-14 16:32:08 +0100

    alter Feature branch

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -12,7 +12,7 @@ class Checkout extends Component{
 
     checkoutContinuedHandler = () => {
         this.props.history.replace("/checkout/contact-data")
-        // this.props.history.push('ezekiel')
+        // changing Feature branch
     }
   
 
